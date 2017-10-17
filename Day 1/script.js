064c58aa1460200f9df3dc1811c72923c2760acf
Wim83 <wimva@mail.com>
2017-10-17 21:07:01 +0200

    everlasting effects

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,17 +1,21 @@
 window.addEventListener('keydown',
-function(e){console.log(e);
-var number = e.keyCode;
-//var audio = document.getElementsByTagName('audio[data-key="'+number+'"]');    DOESN'T WORK!
-var audio = document.querySelector('audio[data-key="'+number+'"]');          // WORKS
-console.log(audio);
-//var audio = new Audio('audio[data-key="'+number+'"]');
-audio.currentTime=0;
-audio.play();
-            //var audio = ('audio[data-key="'+number+'"]');
-//audio.play();
-            //console.log(audio);
-/* <audio data-key="72" src="sounds/ride.wav"></audio> */
+    function (e) {
+        console.log(e);
+        const NUMBER = e.keyCode;
+        // var audio = document.getElementsByTagName('audio[data-key="'+number+'"]');    DOESN'T WORK!
+        // var audio = document.querySelector('audio[data-key="' + NUMBER + '"]'); // WORKS
+        const AUDIO = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+        const KEY = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+        if (!AUDIO) return; // stop function if no 'valid' selection
+        AUDIO.currentTime = 0; // rewind audio (for 'loop' effect)
+        AUDIO.play();
+        console.log(KEY) // testing & visualizing options
+        KEY.classList.add('playing');
+        //var audio = ('audio[data-key="'+number+'"]');
+        //audio.play();
+        //console.log(audio);
+        /* <audio data-key="72" src="sounds/ride.wav"></audio> */
 
-});
+    });
 
 // document.getElementsByTagName('body').addEventListener('click',function(e){console.log(e);})
