06e0a34181af8ae0a2c35ca678083ff818fd2db7
Wim83 <wimva@mail.com>
2017-10-17 20:14:08 +0200

    keyCode trick

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,3 +1,15 @@
-window.addEventListener('keydown',function(e){console.log(e);});
+window.addEventListener('keydown',
+function(e){console.log(e);
+var number = e.keyCode;
+var audio = document.getElementsByTagName('audio[data-key="'+number+'"]')[0];
+console.log(audio);
+var audio = new Audio('audio[data-key="'+number+'"]');
+audio.play();
+            //var audio = ('audio[data-key="'+number+'"]');
+//audio.play();
+            //console.log(audio);
+/* <audio data-key="72" src="sounds/ride.wav"></audio> */
+
+});
 
 // document.getElementsByTagName('body').addEventListener('click',function(e){console.log(e);})
